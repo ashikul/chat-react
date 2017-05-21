bfe68e36f8f01072ef8cb81a3f80371242dd386a
ashikul <alam.ashikul@gmail.com>
2017-05-21 01:57:13 -0400

    trying creat react app

---
+++ chat-make-react/src/Chatbox.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Chatbox extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Chatbox</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Chatbox;
