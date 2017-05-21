517e16b6c7312c0c018c268e32d3bead5d2bee32
ashikul <alam.ashikul@gmail.com>
2017-05-20 21:02:58 -0400

    Initial commit, starting with shopping cart example as a template for react redux

---
+++ src/containers/App.js
@@ -0,0 +1,38 @@
+// import React from 'react'
+// import ProductsContainer from './ProductsContainer'
+// import CartContainer from './CartContainer'
+//
+// const App = () => (
+//   <div>
+//     <h2>Shopping Cart Example</h2>
+//     <hr/>
+//     <ProductsContainer />
+//     <hr/>
+//     <CartContainer />
+//   </div>
+// )
+//
+// export default App
+
+
+import React from 'react'
+import ProductsContainer from './ProductsContainer'
+import CartContainer from './CartContainer'
+//TODO: add chat container???
+
+//TODO: chatbox with props--- user, reciever
+//TODO: user, recievingUser
+
+//TODO: i dont think I need containers. just 2 components...
+//TODO:  possibly... chatbox container... but idk..
+const App = () => (
+    <div>
+        <h2>Shopping Cart Example</h2>
+        <hr/>
+        <ProductsContainer />
+        <hr/>
+        <CartContainer />
+    </div>
+)
+
+export default App
