23e1f2774714f0ac8e92816182cc9235a1aeff2d
ashikul <alam.ashikul@gmail.com>
2017-05-22 17:08:51 -0400

    add moment

--- chat-make-react/src/Chatbox.js
+++ chat-make-react/src/Chatbox.js
@@ -4,7 +4,6 @@ import './styles/Chatbox.css';
 import MessageBox from './MessageBox'
 import ApiService from './ApiService';
 
-console.log('ChatBox');
 class Chatbox extends Component {
 
     constructor (props) {
@@ -21,24 +20,31 @@ class Chatbox extends Component {
         this.handleTextChanged = this.handleTextChanged.bind(this);
         this.handleSendMessage = this.handleSendMessage.bind(this);
         this.handlePressEnter = this.handlePressEnter.bind(this);
+        this.chatHistoryElement = {};
     }
 
     handleSendMessage () {
         if(this.state.message !== '') {
-            this.setState({message: '', buttonDisabled: true})
-            ApiService.pushNewMessageToConversation(this.state.convoId, this.props.user, this.state.message, new Date().toDateString());
+            this.setState({message: '', buttonDisabled: true});
+            ApiService.pushNewMessageToConversation(this.state.convoId, this.props.user, this.state.message, new Date().toString());
             ApiService.setUserIsTyping(this.state.convoId, this.props.user, false);
             console.log(this.state.message);
-            //TODO: add API SERVICE HERE...
-            //TODO: send this to API, add a function here
+            this.chatHistoryElement.scrollTop = this.chatHistoryElement.scrollHeight;
         }
     }
 
     handleTextChanged (evt) {
-        this.setState({message: evt.target.value, buttonDisabled: false});
-        ApiService.setUserIsTyping(this.state.convoId, this.props.user, true);
-        // console.log(ApiService.getReceivingUserIsTyping(1, this.props.receivingUser));
-        //TODO: set is this typing
+        let disableButton;
+        let userIsTyping;
+        if(evt.target.value !== '') {
+            disableButton = false;
+            userIsTyping = true;
+        } else {
+            disableButton = true;
+            userIsTyping = false;
+        }
+        this.setState({message: evt.target.value, buttonDisabled: disableButton});
+        ApiService.setUserIsTyping(this.state.convoId, this.props.user, userIsTyping);
     }
 
     handlePressEnter (evt) {
@@ -47,52 +53,26 @@ class Chatbox extends Component {
         }
     }
 
-    componentWillUpdate() {
-        console.log('componentWillUpdate');
-        console.log(this.state.receivingUserIsTyping);
-        //TODO: this isnt working
-
-        // console.log(this.state.convoId);
-        // console.log(this.state.convoId);
-        // this.setState({
-        //     receivingUserIsTyping: ApiService.getReceivingUserIsTyping(this.state.convoId, this.props.receivingUser),
-        // });
-    }
     componentDidMount () {
-        console.log('componentDidMount');
-        // const userData = ApiService.getUsersData(this.props.user);
-        // console.log('userData');
-        // console.log(userData);
-
         ApiService.setUserOnline(this.props.user);
-        // ApiService.getConvoId(this.props.user, this.props.receivingUser);
         let convoId = ApiService.getConvoId(this.props.user, this.props.receivingUser);
         let messages = ApiService.getMessagesForConversation(convoId);
         let receivingUserIsTyping = ApiService.getReceivingUserIsTyping(convoId, this.props.receivingUser);
-        console.log('receivingUserIsTyping');
-        console.log(receivingUserIsTyping);
 
         this.setState({
             convoId: convoId,
             messages: messages,
             receivingUserIsTyping: receivingUserIsTyping,
         });
-        console.log('messages');
-        console.log(messages);
-        //TODO: get messages≈Ω
 
-        //mocks Socket connection polling
-        this.interval = setInterval(() => this.setState({time: Date.now()}), 1000);
-        // const chatbotContent = document.querySelector('.rsc-content');
-        //
-        // /* istanbul ignore next */
-        // if (chatbotContent) {
-        //     chatbotContent.addEventListener('DOMNodeInserted', this.onNodeInserted);
-        // }
+        //mocks socket connection polling
+        this.interval = setInterval(() => this.setState({
+            time: Date.now(),
+            receivingUserIsTyping: ApiService.getReceivingUserIsTyping(this.state.convoId, this.props.receivingUser)
+        }), 1000);
     }
 
     componentWillUnmount () {
-        console.log('componentWillUnMount');
         ApiService.setUserOffline(this.props.user);
         clearInterval(this.interval);
     }
@@ -102,45 +82,42 @@ class Chatbox extends Component {
             <div className="chatbox">
                 <h4>{this.props.user}'s Chatbox</h4>
 
-                <div className="chatbox-message-history">
+                <div className="chatbox-message-history" ref={(div) => {
+                    this.chatHistoryElement = div
+                }}>
                     {this.state.messages.map((object) =>
                         <MessageBox
                             text={object.message}
                             time={object.time}
-                            user={this.props.user === object.user ? 'You' : object.user}/>
+                            user={object.user}
+                            isYourMessage={this.props.user === object.user} />
                     )}
                 </div>
                 <div className="chatbox-input">
-                    <div className='chat-input'>
-                        <div className='chat-input-core'>
-                            <div className='input-space'>
-                                <input
-                                    placeholder="Type a message..."
-                                    value={this.state.message}
-                                    onChange={this.handleTextChanged}
-                                    onKeyPress={this.handlePressEnter}/>
-                            </div>
-                            <div className='button-space'>
-                                <input
-                                    type='submit'
-                                    className='submit'
-                                    value='Send'
-                                    onClick={this.handleSendMessage}
-                                    disabled={this.state.buttonDisabled}/>
-                            </div>
-                            testing
-                            <br/>
-                            {this.state.receivingUserIsTyping.toString()}
-                            {this.state.convoId}
-                            {this.state.buttonDisabled.toString()}
-                            <br/>
-                            {
-                                this.state.receivingUserIsTyping ?
-                                    <div>true</div> : <div>false</div>
-
-                            }
-                        </div>
+                    <div className="receivingUserIsTypingField">
+                        {
+                            this.state.receivingUserIsTyping ?
+                                <div>{this.props.receivingUser} is typing...</div> : <div></div>
+                        }
                     </div>
+
+                    <div className="input-form">
+                        <input
+                            className='input-message'
+                            placeholder="Type a message..."
+                            value={this.state.message}
+                            onChange={this.handleTextChanged}
+                            onKeyPress={this.handlePressEnter}/>
+                        <input
+                            type='submit'
+                            className='send-button'
+                            value='Send'
+                            onClick={this.handleSendMessage}
+                            disabled={this.state.buttonDisabled}/>
+
+
+                    </div>
+
                 </div>
 
             </div>
@@ -151,7 +128,7 @@ class Chatbox extends Component {
 Chatbox.propTypes = {
     user: PropTypes.string.isRequired,
     receivingUser: PropTypes.string.isRequired,
-}
+};
 
 export default Chatbox;
 
--- chat-make-react/src/MessageBox.js
+++ chat-make-react/src/MessageBox.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './styles/MessageBox.css';
+import moment from 'moment';
 
-const MessageBox = ({text, time, user}) => (
-    <div>
-        <span> {time}|{user}:</span>
-        {text}
+const MessageBox = ({text, time, user, isYourMessage}) => (
+    <div className="MessageBox" style={isYourMessage ? {backgroundColor: 'white'} : {backgroundColor: 'lightblue'}}>
+        <span className="MessageBox-user"> {isYourMessage ? 'You' : user}</span>:
+        <span className="MessageBox-text"> {text} </span>
+        <div className="MessageBox-time">
+            {moment(time).calendar()}
+            {moment(time).format(' h:mm a')}
+        </div>
     </div>
-)
+);
 
 MessageBox.propTypes = {
     text: PropTypes.string,
     time: PropTypes.string,
-    user: PropTypes.string
+    user: PropTypes.string,
+    isYourMessage: PropTypes.bool
 };
+
+moment.updateLocale('en', {
+    calendar: {
+        lastDay: '[Yesterday]',
+        sameDay: '[Today]',
+        nextDay: '[Tomorrow]',
+        lastWeek: '[Last] dddd',
+        nextWeek: '[Next] dddd',
+        sameElse: 'L'
+    }
+});
 
 export default MessageBox
