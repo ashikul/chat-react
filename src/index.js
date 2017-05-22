e30aeaf7f0d79c0fb54ee1f6442380b848b922e3
ashikul <alam.ashikul@gmail.com>
2017-05-22 18:16:19 -0400

    remove old redux starter template, move in new template

--- src/index.js
+++ src/index.js
@@ -1,32 +1,6 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import { createLogger } from 'redux-logger'
-import thunk from 'redux-thunk'
-import reducer from './reducers'
-import { getAllProducts } from './actions'
-import App from './containers/App'
-import '../styles/main.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './styles/index.css';
 
-const middleware = [ thunk ];
-if (process.env.NODE_ENV !== 'production') {
-  middleware.push(createLogger());
-}
-
-const store = createStore(
-  reducer,
-  applyMiddleware(...middleware)
-)
-
-store.dispatch(getAllProducts())
-console.log('rest');
-// TODO: what tochange this to?
-
-// TODO: change to main page
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'));
